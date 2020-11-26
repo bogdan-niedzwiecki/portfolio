b230fcb5f240f2af598d7f64ebb4b29aed9d4661
Bogdan Niedzwiecki <bogdan.niedzwiecki@possible.com>
2020-11-26 17:01:32 +0100

    chunks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
     const social = [{ href: 'https://www.facebook.com/niedzwiecki/', content: 'Fb.' },
     { href: 'https://www.linkedin.com/in/bogdan-niedzwiecki', content: 'Li.' },
     { href: 'https://github.com/bogdan-niedzwiecki', content: 'Gh.' }];
-    
+
     return (
       <footer className="footer">
         <ul className="social">
